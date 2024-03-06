e4e7035e9975e50095cf17010a9e419e795c23ec
LinaYahya <linaebe0@gmail.com>
2024-03-06 16:56:07 +0100

    feat: wrap page with the context

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -16,6 +16,7 @@ import { useAnalyticsTranslation } from '@/config/i18n';
 import { HOME_PATH } from '@/config/paths';
 
 import UserSwitchWrapper from '../common/UserSwitchWrapper';
+import ContextsWrapper from '../context/ContextsWrapper';
 import Footer from './Footer';
 import Navigator from './Navigator';
 import Sidebar from './Sidebar';
@@ -55,37 +56,40 @@ const PageWrapper = ({ children }: { children: JSX.Element }): JSX.Element => {
       ...getNavigationEvents(Platform.Analytics),
     },
   };
+
   return (
-    <Main
-      context={Context.Analytics}
-      drawerContent={<Sidebar />}
-      drawerOpenAriaLabel={t('DRAWER_OPEN_ARIA')}
-      headerRightContent={<UserSwitchWrapper />}
-      PlatformComponent={
-        <PlatformSwitch
-          selected={Platform.Analytics}
-          platformsProps={platformProps}
-          disabledColor="#999"
-        />
-      }
-      LinkComponent={LinkComponent}
-    >
-      <Box
-        height="100%"
-        display="flex"
-        flexGrow={1}
-        justifyContent="space-between"
-        flexDirection="column"
-        // counteract the footer
-        pb="64px"
+    <ContextsWrapper>
+      <Main
+        context={Context.Analytics}
+        drawerContent={<Sidebar />}
+        drawerOpenAriaLabel={t('DRAWER_OPEN_ARIA')}
+        headerRightContent={<UserSwitchWrapper />}
+        PlatformComponent={
+          <PlatformSwitch
+            selected={Platform.Analytics}
+            platformsProps={platformProps}
+            disabledColor="#999"
+          />
+        }
+        LinkComponent={LinkComponent}
       >
-        <Box id="navigatorContainer" width="100%">
-          <Navigator />
+        <Box
+          height="100%"
+          display="flex"
+          flexGrow={1}
+          justifyContent="space-between"
+          flexDirection="column"
+          // counteract the footer
+          pb="64px"
+        >
+          <Box id="navigatorContainer" width="100%">
+            <Navigator />
+          </Box>
+          {children}
+          <Footer />
         </Box>
-        {children}
-        <Footer />
-      </Box>
-    </Main>
+      </Main>
+    </ContextsWrapper>
   );
 };
 export default PageWrapper;
